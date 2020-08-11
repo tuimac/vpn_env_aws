305444c522ff4e5346b8cb3f27ceb351cc192994
tuimac <tuimac.devadm01@gmail.com>
2020-08-11 23:44:11 +0900

    fixed

--- web/downloadFistPem.py
+++ web/downloadFistPem.py
@@ -2,21 +2,28 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
+import os
+import sys
+import time
 import traceback
 
 class DFPem:
-
     @staticmethod
     def run(ip="0.0.0.0", port=80):
+        filename = 'test/test.txt'
         try:
             class ServerHandler(BaseHTTPRequestHandler):
                 def do_GET(self):
-                    param = urlparse(self.path)
-                    query = parse_qs(param.query)
-                    print(param)
-                    print(query)
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/plain; charset=utf-8")
+                    self.end_headers()
+                    with open(filename, 'rb') as f:
+                        self.wfile.write(f.read())
+                    os.remove(filename)
 
-            self.httpServer = HTTPServer((ip, port), ServerHandler)
-            self.httpServer.serve_forever()
+            httpServer = HTTPServer((ip, port), ServerHandler)
+            httpServer.serve_forever()
+        except FileNotFoundError:
+            sys.exit(1)
         except:
             traceback.print_exc()
